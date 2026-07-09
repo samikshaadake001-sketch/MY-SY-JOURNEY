4ec9e01931cf9ce4e2ac71bddb4a1a7fadf7699d
unknown <samikshaadake001@gmail.com>
2026-07-10 00:30:19 +0530

    My first code of C++

---
+++ c++ practice/1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,d,e,f;
+    cout<<"Enter first number:";
+    cin>>a;
+    cout<<"Enter second number:";
+    cin>>b;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    cout<<"Answer:"<<c<<"\n";
+     cout<<"Answer:"<<d<<"\n";
+     cout<<"Answer:"<<e<<"\n";
+      cout<<"Answer:"<<f<<"\n";
+}
